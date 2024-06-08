e9c91cfe33ea1b4a585006968744fbd315f9f0e3
roxtraAK <alexander.krinke@roxtra.com>
2024-06-08 14:30:19 +0200

    responsive header

--- src/components/layout/Header.tsx
+++ src/components/layout/Header.tsx
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { useState, useEffect, useMemo } from "react";
 import AppBar from "@mui/material/AppBar";
 import Box from "@mui/material/Box";
 import Toolbar from "@mui/material/Toolbar";
@@ -17,17 +17,25 @@ import Divider from "@mui/material/Divider";
 import MailIcon from "@mui/icons-material/Mail";
 import Menu from "@mui/material/Menu";
 import MenuItem from "@mui/material/MenuItem";
-import { useMobile } from "../../Hooks/useMobile";
 import EmojiEventsIcon from "@mui/icons-material/EmojiEvents";
 import FitnessCenterIcon from "@mui/icons-material/FitnessCenter";
 import DescriptionIcon from "@mui/icons-material/Description";
 import styles from "../../styles/style.module.css";
-import { Link, useNavigate } from "react-router-dom";
+import { Link } from "react-router-dom";
 
 export function Header() {
   const [open, setOpen] = useState(false);
   const [anchorElUser, setAnchorElUser] = useState(null);
-  const isMobile = useMobile();
+  const [width, setWidth] = useState(window.innerWidth);
+
+  useEffect(() => {
+    const handleResize = () => {
+      setWidth(window.innerWidth);
+    };
+
+    window.addEventListener("resize", handleResize);
+    return () => window.removeEventListener("resize", handleResize);
+  }, []);
 
   const pages = ["Home", "Coaching", "Erfolge", "Impressum", "Kontakt"];
   const settings = ["Profile", "Account", "Dashboard", "Logout"];
@@ -43,7 +51,7 @@ export function Header() {
   const DrawerList = (
     <Box
       sx={{
-        width: "250",
+        width: 250,
         height: "100%",
         backgroundColor: "black",
         color: "white",
@@ -57,7 +65,7 @@ export function Header() {
           <ListItem key={text} component={Link} to={text.toLowerCase()}>
             <ListItemButton>
               <ListItemIcon>
-                {text == "Coaching" ? (
+                {text === "Coaching" ? (
                   <FitnessCenterIcon color="primary" key={`navbar-${index}`} />
                 ) : text === "Erfolge" ? (
                   <EmojiEventsIcon color="primary" key={`navbar-${index}`} />
@@ -65,7 +73,7 @@ export function Header() {
                   <MailIcon color="primary" key={`navbar-${index}`} />
                 ) : text === "Impressum" ? (
                   <DescriptionIcon color="primary" key={`navbar-${index}`} />
-                ) : undefined}
+                ) : null}
               </ListItemIcon>
               <ListItemText primary={text} sx={{ fontFamily: "favela" }} />
             </ListItemButton>
@@ -75,6 +83,16 @@ export function Header() {
       <Divider />
     </Box>
   );
+
+  const displayType = useMemo(() => {
+    if (width < 900) {
+      return "menuOnly";
+    } else if (width < 1100) {
+      return "menuWithPages";
+    } else {
+      return "full";
+    }
+  }, [width]);
 
   return (
     <AppBar
@@ -90,26 +108,7 @@ export function Header() {
     >
       <Container maxWidth={false} sx={{ padding: 0 }}>
         <Toolbar disableGutters>
-          <Typography
-            variant="h6"
-            noWrap
-            component="a"
-            href="/"
-            sx={{
-              display: { xs: "none", md: "flex" },
-              fontWeight: 700,
-              fontSize: "40px",
-              fontFamily: "favela",
-              letterSpacing: ".3rem",
-              color: "white",
-              textDecoration: "none",
-              marginLeft: 2,
-            }}
-          >
-            Fabio Willmann
-          </Typography>
-
-          {isMobile ? (
+          {displayType === "menuOnly" ? (
             <>
               <IconButton
                 color="inherit"
@@ -120,25 +119,47 @@ export function Header() {
               >
                 <MenuIcon />
               </IconButton>
-              <Typography
-                variant="h6"
-                noWrap
-                component="a"
-                href="/"
-                sx={{
-                  position: "absolute",
-                  left: { xs: "55%", sm: "50%" },
-                  transform: "translateX(-50%)",
-                  fontWeight: 700,
-                  fontSize: { xs: "30px" },
-                  fontFamily: "favela",
-                  letterSpacing: ".3rem",
-                  color: "white",
-                  textDecoration: "none",
-                }}
+              <Drawer
+                anchor="left"
+                open={open}
+                onClick={toggleDrawer()}
+                onClose={toggleDrawer()}
               >
-                Fabio Willmann
-              </Typography>
+                {DrawerList}
+              </Drawer>
+            </>
+          ) : displayType === "menuWithPages" ? (
+            <>
+              <IconButton
+                color="inherit"
+                aria-label="open drawer"
+                onClick={toggleDrawer()}
+                edge="start"
+                sx={{ marginLeft: 1 }}
+              >
+                <MenuIcon />
+              </IconButton>
+              {pages.map((page, index) => (
+                <Button
+                  key={`topbar-${index}`}
+                  sx={{
+                    my: 3,
+                    fontWeight: 700,
+                    fontFamily: "favela",
+                    fontSize: "20px",
+                    color: "white",
+                    display: "block",
+                    mr: 2,
+                    transition: "transform 0.2s ease",
+                    "&:hover": {
+                      transform: "scale(1.2)",
+                      backgroundColor: "black",
+                    },
+                  }}
+                >
+                  {page}
+                </Button>
+              ))}
               <Drawer
                 anchor="left"
                 open={open}
@@ -153,10 +174,26 @@ export function Header() {
               sx={{
                 flexGrow: 0.8,
                 display: "flex",
-                justifyContent: "center",
                 alignItems: "center",
               }}
             >
+              <Typography
+                variant="h6"
+                noWrap
+                component="a"
+                href="/"
+                sx={{
+                  display: { xs: "none", md: "flex" },
+                  fontWeight: 700,
+                  fontSize: "40px",
+                  fontFamily: "favela",
+                  letterSpacing: ".3rem",
+                  color: "white",
+                  textDecoration: "none",
+                }}
+              >
+                Fabio Willmann
+              </Typography>
               {pages.map((page, index) => (
                 <Button
                   key={`topbar-${index}`}
